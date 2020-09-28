e1ec6cb14cf8d53cdf544e9056b1593cc792c5fb
Umair Hussein <umahus@melinmedical.com>
2020-09-28 21:26:55 +0200

    write console log

--- src/UNMealPlanner/Program.cs
+++ src/UNMealPlanner/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Blazored.LocalStorage;
 using Blazored.Modal;
 using UNMealPlanner.Services;
@@ -18,20 +14,23 @@ namespace UNMealPlanner
     {
         public static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            try
+            {
+                var builder = WebAssemblyHostBuilder.CreateDefault(args);
+                builder.RootComponents.Add<App>("app");
+                builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+                builder.Services.AddBlazoredToast();
+                builder.Services.AddBlazoredModal();
+                builder.Services.AddBlazoredLocalStorage();
+                builder.Services.AddScoped<IMealService, MealService>();
+                await builder.Build().RunAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-            builder.Services.AddBlazoredToast();
-
-            builder.Services.AddBlazoredModal();
-
-            builder.Services.AddBlazoredLocalStorage();
-
-            builder.Services.AddScoped<IMealService, MealService>();
-
-            await builder.Build().RunAsync();
+                throw;
+            }
         }
     }
 }
