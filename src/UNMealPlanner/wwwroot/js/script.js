28e5910bf79818cff30cdaba091eddfb7248186f
Umair Syed <naqvi.umair@gmail.com>
2020-10-09 16:11:15 +0200

    dragmove calender style2

--- src/UNMealPlanner/wwwroot/js/script.js
+++ src/UNMealPlanner/wwwroot/js/script.js
@@ -9,23 +9,64 @@ function goForward() {
     window.history.forward();
 }
 
-
 $(document).ready(function () {
     if (!!mybutton) {
         mybutton.style.display = "none";
     }
-
-    //var checkExist = setInterval(function () {
-    //    var today = document.getElementById("todayMobile");
-    //    console.log('checking today');
-
-    //    if (!!today) {
-    //        console.log('found today');
-    //        today.scrollIntoView();
-    //        clearInterval(checkExist);
-    //    }
-    //}, 100);
 });
+
+function CollapseNavBar() {
+    $('.navbar-collapse').collapse('hide');
+}
+
+function CheckQuickLinks() {
+    var checkExist = setInterval(function () {
+        var gallery = document.getElementById("gallery");
+        //console.log('checking gallery');
+
+        if (!!gallery) {
+            $('#gallery').flickity({
+                cellAlign: 'left',
+                freeScroll: true,
+                contain: true,
+                pageDots: false,
+                prevNextButtons: false,
+                resize: true,
+            });
+            clearInterval(checkExist);
+        }
+    }, 100);
+}
+
+function CheckCalDays() {
+    var checkExist = setInterval(function () {
+        var gallery = document.getElementById("CalDays");
+        //console.log('checking gallery');
+
+        if (!!gallery) {
+            var n = new Date().getDate();
+            
+            //console.log('Today Day ' + n);
+
+            if (n - 1 < 0) {
+                n = 0
+            }
+
+            $('#CalDays').flickity({
+                cellAlign: 'left',
+                freeScroll: true,
+                contain: true,
+               // wrapAround: true,
+                pageDots: false,
+                prevNextButtons: false,
+                resize: false,
+                initialIndex: n-1,
+                friction: 0.08,
+            });
+            clearInterval(checkExist);
+        }
+    }, 100);
+}
 
 function CheckForToday() {
     var checkExist = setInterval(function () {
