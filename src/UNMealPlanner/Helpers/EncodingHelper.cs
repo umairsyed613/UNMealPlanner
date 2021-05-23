b01cbb086fae3f543cda57393a1ad8e44b9a7247
Umair Syed <naqvi.umair@gmail.com>
2021-05-23 21:08:33 +0200

    share grocery list

---
+++ src/UNMealPlanner/Helpers/EncodingHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace UNMealPlanner.Helpers
+{
+    public static class EncodingHelper
+    {
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
